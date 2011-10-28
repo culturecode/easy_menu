e7926bedbd93d51c3496d7565a1e29de02f8e81b
Nicholas Jakobsen <nicholas.jakobsen@telus.net>
2011-10-28 16:36:44 -0700

    Fixed remote button form submit bug that prevented the commit value from being included.

--- app/assets/javascripts/easy_menu.js
+++ app/assets/javascripts/easy_menu.js
@@ -3,7 +3,10 @@ $(document).ready(function() {
     // do it in javascript just to make sure it happens
     $('input[type=submit][form]').click(function(){
         if (form = document.getElementById(this.getAttribute('form'))){
-            form.submit();
+            var tempCommit = $("<input type='submit' name='commit' style='display:none'>");
+            tempCommit.attr('value', $(this).attr('value'));
+            form.appendChild(tempCommit[0]);
+            tempCommit.click();
             return false;
         }
     });                    
