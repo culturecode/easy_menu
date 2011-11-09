0c4f64228c36de041cf80ded492763e20c93272c
Ryan Wallace <rywall@gmail.com>
2011-11-08 16:02:07 -0800

    fix submit of form when cancel is pressed.

--- app/assets/javascripts/easy_menu.js
+++ app/assets/javascripts/easy_menu.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
     // Because some browsers don't support submit buttons outside of forms triggering form submits,
     // do it in javascript just to make sure it happens
-    $('input[type=submit][form]').click(function(){
-        if (form = document.getElementById(this.getAttribute('form'))){
+    $('input[type=submit][form]').live('click', function(){
+        if (form = document.getElementById(this.getAttribute('form'))) {
+            // If the form is already submitting, we want don't want to submit it again.
+            if ($(form).hasClass('submitting')) {
+              return false;
+            }
+            
             var tempCommit = $("<input type='submit' name='commit' style='display:none'>");
             tempCommit.attr('value', $(this).attr('value'));
             form.appendChild(tempCommit[0]);
             tempCommit.click();
             return false;
         }
+    });
+    // Used by the above click handler to determine if the browser already submitted the form.
+    $('form').submit(function() {
+      $(this).addClass('submitting');
     });                    
 });
