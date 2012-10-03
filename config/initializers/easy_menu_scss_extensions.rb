c761a4b8dd2b9e19abc8d1c3a78644a190eb446b
Nicholas Jakobsen <nicholas.jakobsen@telus.net>
2012-10-03 12:42:17 -0700

    Improved gradient support in IE9, remove gradient support in IE8 because it used ugly filters.

---
+++ config/initializers/easy_menu_scss_extensions.rb
@@ -0,0 +1,18 @@
+require 'base64'
+
+# tools.ietf.org/html/rfc2397
+# developer.mozilla.org/en/data_URIs
+
+module Sass::Script::Functions
+  def data_url(content_type, content)
+    outuri = "data:#{unquote(content_type)};base64,#{Base64.encode64(unquote(content).to_s)}"
+
+    # IE8 has a 32KiB limit on data uri
+    # en.wikipedia.org/wiki/Data_URI_scheme
+    if outuri.length > 32768
+      raise ArgumentError.new("Data URI is greater than 32KiB in size, that is the max size of data urls in IE8.")
+    end
+    
+    Sass::Script::String.new("url('#{outuri}')")
+  end
+end
